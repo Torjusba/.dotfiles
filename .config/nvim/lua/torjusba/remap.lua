21359c1b08b3dcfdb054ce9174810cdd9c8f1866
Torjus Bakkene <torjus.bakkene@gmail.com>
2023-03-16 20:02:08 +0100

    <leader>ec executes current python file or rust project

--- .config/nvim/lua/torjusba/remap.lua
+++ .config/nvim/lua/torjusba/remap.lua
@@ -2,3 +2,14 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>ec", function ()
+    local filetype = vim.bo.filetype
+    if filetype == "python" then
+        vim.cmd('!python %')
+    elseif filetype == "rust" then
+        vim.cmd('cargo run')
+    else
+        print("Execute current is not implemented for filetype", vim.bo.filetype)
+    end
+end)
